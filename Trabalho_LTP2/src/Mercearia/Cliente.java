3b585ce2dc67153d81236a340df57fd9094176a2
GustavoMiguel2001 <62038158+GustavoMiguel2001@users.noreply.github.com>
2020-06-16 18:57:54 -0300

    Update Cliente.java

--- Trabalho_LTP2/src/Mercearia/Cliente.java
+++ Trabalho_LTP2/src/Mercearia/Cliente.java
@@ -115,6 +115,123 @@ public class cliente {
 	}
 
 	public void atualizar(cliente cliente) {
+package Cliente2;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class cliente {
+	private int id;
+	private String NomeCliente;
+	private String TelefoneCliente;
+	private String CPF;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return NomeCliente;
+	}
+
+	public void setNome(String NomeCliente) {
+		this.NomeCliente = NomeCliente;
+	}
+
+	public String TelefoneCliente() {
+		return TelefoneCliente;
+	}
+
+	public void setTelefoneCliente(String TelefoneCliente) {
+		this.TelefoneCliente = TelefoneCliente;
+	}
+
+	public String CPF() {
+		return CPF;
+	}
+
+	public void setCPF(String CPF) {
+		this.CPF = CPF;
+	}
+
+	public void inserir(cliente cliente) {
+
+		try {
+
+			Connection connection = conexaoUtil.getInstance().getConnection();
+
+			String sql = "INSERT INTO PRODUTO (NOMECLIENTE, TELEFONECLIENTE, CPF) VALUES(?,?,?)";
+
+			PreparedStatement statement = connection.prepareStatement(sql);
+
+			statement.setString(1, cliente.getNome());
+			statement.setString(2, cliente.TelefoneCliente());
+			statement.setString(3, cliente.CPF());
+
+			statement.execute();
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void remover(int id) {
+		try {
+
+			Connection connection = conexaoUtil.getInstance().getConnection();
+
+			String sql = "DELETE FROM PRODUTO WHERE ID = ?";
+			PreparedStatement statement = connection.prepareStatement(sql);
+
+			statement.setInt(1, id);
+
+			statement.execute();
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public List<cliente> listarTodos() {
+		List<cliente> listaClientes = new ArrayList<cliente>();
+		try {
+			Connection connection = conexaoUtil.getInstance().getConnection();
+
+			String sql = "SELECT * FROM PRODUTO";
+
+			PreparedStatement statement = connection.prepareStatement(sql);
+
+			ResultSet resultset = statement.executeQuery();
+
+			while (resultset.next()) {
+				cliente p = new cliente();
+				p.setId(resultset.getInt("ID"));
+				p.setNome(resultset.getString("NOME"));
+				p.setTelefoneCliente(resultset.getString("TELEFONE"));
+				p.setCPF(resultset.getString("CPF"));
+
+				listaClientes.add(p);
+			}
+			connection.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return listaClientes;
+	}
+
+	public void atualizar(cliente cliente) {
 		try {
 			Connection connection = conexaoUtil.getInstance().getConnection();
 
