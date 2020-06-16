0468466be25ac9b899dfba614b67229330161d87
Arthur-S-Souza <50931026+Arthur-S-Souza@users.noreply.github.com>
2020-06-16 19:50:31 -0300

    Update Produto.java

--- Trabalho_LTP2/src/Mercearia/Produto.java
+++ Trabalho_LTP2/src/Mercearia/Produto.java
@@ -1,11 +1,9 @@
-package master_class;
-
-import library.Utils.conexaoUtil;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+import library.ConexaoUtil;;
 
 public class Produto {
 	private int id;
@@ -53,12 +51,14 @@ public class Produto {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+	
+	public static class dados {
 
 	public void inserir(Produto Produto) {
 
 		try {
 
-			Connection connection = conexaoUtil.getInstance().getConnection();
+			Connection connection = ConexaoUtil.getConnection();
 
 			String sql = "INSERT INTO PRODUTO (NOME, VALOR, CATEGORIA, DESCRICAO) VALUES(?,?,?,?)";
 
@@ -71,6 +71,8 @@ public class Produto {
 
 			statement.execute();
 			connection.close();
+			
+			System.out.println("Inserido com sucesso!");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -80,68 +82,88 @@ public class Produto {
 	public void remover(int id) {
 		try {
 
-			Connection connection = conexaoUtil.getInstance().getConnection();
+			Connection connection = ConexaoUtil.getConnection();
 
 			String sql = "DELETE FROM PRODUTO WHERE ID = ?";
 			PreparedStatement statement = connection.prepareStatement(sql);
-			
+
 			statement.setInt(1, id);
-			
+
 			statement.execute();
 			connection.close();
+			
+			System.out.println("Removido com sucesso!");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
 	}
+
 	public List<Produto> listarTodos() {
 		List<Produto> listaProdutos = new ArrayList<Produto>();
 		try {
-			Connection connection = conexaoUtil.getInstance().getConnection();
-			
+			Connection connection = ConexaoUtil.getConnection();
+
 			String sql = "SELECT * FROM PRODUTO";
-			
+
 			PreparedStatement statement = connection.prepareStatement(sql);
-			
+
 			ResultSet resultset = statement.executeQuery();
-			
-			while(resultset.next()) {
+
+			while (resultset.next()) {
 				Produto p = new Produto();
 				p.setId(resultset.getInt("ID"));
 				p.setNome(resultset.getString("NOME"));
 				p.setValor(resultset.getFloat("VALOR"));
 				p.setCategoria(resultset.getString("CATEGORIA"));
 				p.setDescricao(resultset.getString("DESCRICAO"));
-				
+
 				listaProdutos.add(p);
 			}
 			connection.close();
-			
-		}catch (Exception e) {
+			System.out.println("Lista mostrada com sucesso!");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return listaProdutos;
 	}
-	
-	public void atualizar (Produto produto) {
+
+	public void atualizar(Produto produto) {
 		try {
-			Connection connection = conexaoUtil.getInstance().getConnection();
-			
+			Connection connection = ConexaoUtil.getConnection();
+
 			String sql = "UPDATE PRODUTO SET NOME = ?, VALOR = ?, CATEGORIA = ?, DESCRICAO = ? WHERE ID = ?";
-			
+
 			PreparedStatement statement = connection.prepareStatement(sql);
 
 			statement.setString(1, produto.getNome());
 			statement.setFloat(2, produto.getValor());
 			statement.setString(3, produto.getCategoria());
 			statement.setString(4, produto.getDescricao());
-
 			
-		}catch (Exception e){
+			statement.execute();
+			statement.close();
+			
+			System.out.println("Atualizado com sucesso!");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return;
 	}
 
-}
+		public static void main (String[] args) {
+			Produto p = new Produto();
+			
+			p.setNome("Arthur");
+			p.setValor(36);
+			p.setCategoria("Qualquer");
+			p.setDescricao("Nada");
+			
+			dados s = new dados();
+			
+			s.inserir(p);
+			
+			}
+		}
+	}
